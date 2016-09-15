54563112c7d84fbde1e5b28f87ec040485bb44a4
andrewdhicks <andrew.hicks@ga.gov.au>
2016-09-15 14:42:51 +1000

    Update build setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 setup(name='ndvi',
       version=os.environ.get('version', 0.0),
       description='Geoscience Australia - NDVI for AGDC',
-      long_description=open('README.md', 'r').read(),
+      long_description=open('README.rst', 'r').read(),
       license='Apache License 2.0',
       url='https://github.com/GeoscienceAustralia/ndvi',
       author='AGDC Collaboration',
