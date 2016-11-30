8f71536eef48df6b3005b91631d8c2b5fdb63891
Andrew Hicks <andrew.hicks@ga.gov.au>
2016-12-01 10:32:53 +1100

    Fix command line args for queue_size and bokeh

--- ndvi/ndvi_app.py
+++ ndvi/ndvi_app.py
@@ -200,7 +200,7 @@ APP_NAME = 'ndvi'
 @ui.pass_index(app_name=APP_NAME)
 @click.option('--dry-run', is_flag=True, default=False, help='Check if output files already exist')
 @click.option('--year', callback=validate_year, help='Limit the process to a particular year')
-@click.option('--queue-size', '--backlog', type=click.IntRange(1, 100000), default=3200,
+@click.option('--queue-size', type=click.IntRange(1, 100000), default=3200,
               help='Number of tasks to queue')
 @task_app_options
 @task_app(make_config=make_ndvi_config, make_tasks=make_ndvi_tasks)
